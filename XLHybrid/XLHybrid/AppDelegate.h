43919d2c77d22019bf2d919500ccdeeba5dbe118
wangxianliang <wangxianliang@jinrongweb.com>
2018-03-21 11:09:39 +0800

    初始化项目

---
+++ XLHybrid/XLHybrid/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XLHybrid
+//
+//  Created by xlCoder on 2018/3/21.
+//  Copyright © 2018年 Xianliang_Mr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XLHybrid/XLHybrid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XLHybrid
+//
+//  Created by xlCoder on 2018/3/21.
+//  Copyright © 2018年 Xianliang_Mr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
